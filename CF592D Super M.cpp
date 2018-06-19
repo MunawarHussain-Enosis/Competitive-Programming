07b47ee3bbd796ac096efb08349a8dea25fd4b36
Munawar Hussain Shakil <munawar.hussain@enosisbd.com>
2018-06-19 19:16:39 +0600

    CF592 added

---
+++ CF592D Super M.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define LL long long int
+#define ULL unsigned long long int
+#define sf(a) scanf("%d",&a)
+#define sl(a) scanf("%lld",&a)
+#define fr first
+#define sc second
+#define pii pair<int,int>
+#define pll pair<LL,LL>
+#define vi vector<int>
+#define vll vector<LL>
+#define vpii  vector<pii>
+#define rep1(a,b) for(int a=1;a<=b;a++)
+#define rep2(a,b) for(int a=0;a<b;a++)
+#define CLR(a,b) memset(a,b,sizeof(a))
+#define Clear(a,b) memset(a,b,sizeof(a))
+#define pb push_back
+#define mp make_pair
+#define mod 1000000007
+#define  LSOne(S) (S & (-S))
+#define all(a) a.begin(),a.end()
+#define Prime 31
+using namespace std;
+#define maxn 200100
+#define INF 1<<29
+#define inf 1ll<<62
+#define mMax 20005
+#define nMax 2505
+#define SZ(a) a.size()
+#define  Unique(A) (A.resize(distance(A.begin(),unique(all(A)))))
+void printVector(vi a) {for(int ii=0;ii<SZ(a);ii++) cout<<a[ii]<<endl;}
+std::vector<vi> edges;
+struct dp[3][123457];
+int marked[123457];
+
+int n,m;
+void dfs(int u,int par){
+	for(auto v:edges[u]){
+		if(v==par) continue;
+		dfs(v,u);
+		marked[u] += marked[v];
+	}
+	return;
+}
+
+
+
+int main()
+{
+	cin>>n>>m;
+	edges.assign(n+1,vi());
+	int u,v;
+	rep1(i,n-1){
+		scanf("%d %d",&u,&v);
+		edges[u].pb(v);
+		edges[v].pb(u);
+	}
+	rep1(i,m){
+		scanf("%d",&u);
+		marked[u] = 1;
+	}
+	dfs(1,0);
+    return 0;
+}
